6eaf9a2e6cb73a14152ecffc540f3172b0f1e15e
mati <matiCIF@outlook.es>
2021-05-11 13:56:28 +0200

    c1

--- GestionGastos/src/Login.java
+++ GestionGastos/src/Login.java
@@ -4,5 +4,12 @@ public class Login {
 	//pedir al usuario nombre y contraseña
 	//con sus mensajes por pantalla
 	
-
+	
+	//test first commit
+	
+	public static void login() {
+		
+		System.out.print("Hi all");
+	}
+	
 }
