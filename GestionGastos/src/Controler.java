9f4b48f8317aa2c6453ff463345b77c91853b6f5
mati <mati@DESKTOP-GAKM3OF>
2021-05-11 13:47:58 +0200

    Commit 1 bienvenida y primera pregunta

--- GestionGastos/src/Controler.java
+++ GestionGastos/src/Controler.java
@@ -5,20 +5,34 @@ public class Controler {
 
 	public static void Start () {
 	
-Scanner reader = new Scanner(System.in);
-	
-	System.out.println("Bienvenido/a!! Introduce tu nombre:");
-	String nombre1 = reader.nextLine();
-	
-	
 
-		CreacionCuenta.crearCuenta("cuenta");
-		//GestionGastos.añadirGastos
+		//Dar bienvenida. Preguntar si es nuevo/a. Si no lo es, dar paso a Login.
+		//Crear el while
+		
+	
+	Scanner reader = new Scanner(System.in);
+
+	System.out.println("Bienvenido/a a tu espacio de gestión de gastos. ¿Eres nuevo/aquí?");
+	String respuesta1 = reader.nextLine();
+	
+	if (respuesta1.equals("Si")){
+		
+		System.out.println ("¡Pues vamos a crearte una!");
+		//CreacionCuenta.crearCuenta();
+	}
+	
+	else {
+		
+		//Login.login();
+		System.out.println ("¡Pues vamos a loguearte!");
+	}
+		
+		
+	}//GestionGastos.añadirGastos
 		//GestionGastos.modificarGastos
 		//GestionGastos.borrarGastos
 		//GestionGastos.listarGastos
 		//GestionGastos.salir
-		reader.close();
-	}
+		//reader.close();
 }
 
