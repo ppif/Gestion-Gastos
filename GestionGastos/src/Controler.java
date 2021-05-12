59a1955d860a797a811476a30f70c6400c81c0e9
mati <mati@DESKTOP-GAKM3OF>
2021-05-12 10:37:23 +0200

    commit controler acabado de momento

--- GestionGastos/src/Controler.java
+++ GestionGastos/src/Controler.java
@@ -1,42 +1,53 @@
 import java.util.Scanner;
-import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Controler {
 
 	public static void Start () {
 	
-	//Crear el while
-	//Dar bienvenida. Preguntar si es nuevo/a. Si no lo es, dar paso a Login.
+	//Declarar nuevo objeto tipo scanner llamado reader para interactuar con cliente.
+	//Declarar nuevo objeto de tipo Arraylist de users.
+	//Crear el while	
+	//String de bienvenida: Dar opciones menú (login, createuser...)
+	//Crear los ifs	
 	
 	
+		Scanner reader = new Scanner(System.in);
+		//ArrayList<User> users = new ArrayList<User>();
+		
 		
 		while (true) {	
 	
-	Scanner reader = new Scanner(System.in);
 
-	System.out.println("Bienvenido/a a tu espacio de gestión de gastos. ¿Eres nuevo/a aquí? Responde Quit en caso de que quieras salir de él.");
-	String respuesta1 = reader.nextLine();
+			System.out.println("Bienvenido/a a tu espacio de gestión de gastos. ¿Eres nuevo/a aquí? Responde Quit en caso de que quieras salir.");
+			
+			String respuesta1 = reader.nextLine();
 	
 	if (respuesta1.equals("Si")){
 		
-		System.out.println ("¡Pues vamos a crearte una!");
+		System.out.println ("¡Pues vamos a crearte una cuenta!");
 		//CreacionCuenta.crearCuenta();
 	}
 	
 	else if (respuesta1.equals("No")) {
-		
+		System.out.println ("¡Pues entremos en tu espacio personal!");
 		//Login.login();
 	}
 		
 		
 	else if (respuesta1.equals("Quit")) {
+		break;
 	
-	break;
+	}
+	
+	else{		
+		System.out.println ("Disculpa, no te he entendido. ¿Podrías repetir?");
 	
 	}
 	
 	}	
-		
+	reader.close();	
 	}
 }
 
@@ -45,4 +56,4 @@ public class Controler {
 		//GestionGastos.borrarGastos
 		//GestionGastos.listarGastos
 		//GestionGastos.salir
-		//reader.close();
+		
