f9470bf618e07ad2c4be76d1e2882b3db39414a2
mati <mati@DESKTOP-GAKM3OF>
2021-05-19 13:49:51 +0200

    commit 2

--- GestionGastos/src/controler/ControlerPrincipal.java
+++ GestionGastos/src/controler/ControlerPrincipal.java
@@ -1,28 +1,28 @@
 package controler;
+
+
+import modelo.CreacionUsuario;
+import modelo.Usuario;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class ControlerPrincipal {
 
+	// Creamos objeto de tipo Arraylist de usuarios.
 	public static ArrayList<Usuario> listOfUsers = new ArrayList<>();
 
+	// Creamos método de inicio del programa:
 	public static void Start() {
 
-		// Declarar nuevo objeto tipo scanner llamado reader para interactuar con
-		// cliente.
-		// Declarar nuevo objeto de tipo Arraylist de users.
-		// Crear el while
-		// String de bienvenida: Dar opciones menÃº (login, createuser...)
-		// Crear los ifs
-		//
-		//
-
+		// Declaramos nuevo objeto scanner (reader) para interactuar con cliente.
 		Scanner reader = new Scanner(System.in);
-		
 
+		// Creamos Menu Principal con: mensaje de bienvenida y opciones de elección:
+		// (crear cuenta, entrar en mi espacio, salir)
 		while (true) {
 
-			// MenÃº de Opciones
+			// Menu Principal:
 			System.out.println("BIENVENIDO A TU PLATAFORMA DE GESTION DE GASTOS.");
 			System.out.println("Por favor, introduce la opcion que desees llevar a cabo");
 			System.out.println("Crear cuenta");
@@ -31,18 +31,17 @@ public class ControlerPrincipal {
 
 			String respuesta1 = reader.nextLine();
 
-			// Condiciones con sus caminos a metodos
+			// Condiciones con sus caminos a metodos (para ejecutar la eleccion del cliente)
 			if (respuesta1.equalsIgnoreCase("Crear cuenta")) {
 				System.out.println("¡Vamos a crearte una cuenta!");
-			
+
 				CreacionUsuario.crearCuenta();
-				//GestionGastos.MenuPrincipalGestionGastos(); // esta llamada a mÃ©todo aquÃ­ es provisional, estoy probando
-															// que funcione.
+
 			}
 
 			else if (respuesta1.equalsIgnoreCase("Entrar en mi espacio")) {
 				System.out.println("Â¡Entremos en tu espacio personal!");
-			    LoginControler.login();
+				LoginControler.login();
 			}
 
 			else if (respuesta1.equalsIgnoreCase("Salir")) {
@@ -57,12 +56,10 @@ public class ControlerPrincipal {
 
 		}
 		reader.close();
-		
-		
-		
+
 	}
 	// Metodos que trabajan con el Arraylist
-	
+
 	public static boolean checkIfUserExist(String user) {
 		for (int i = 0; i < listOfUsers.size(); i++) {
 			if (listOfUsers.get(i).getUsername().equals(user)) {
@@ -71,7 +68,7 @@ public class ControlerPrincipal {
 		}
 		return false;
 	}
-	
+
 	public static Usuario getUserFromList(String user) {
 		for (int i = 0; i < listOfUsers.size(); i++) {
 			if (listOfUsers.get(i).getUsername().equals(user)) {
@@ -80,10 +77,10 @@ public class ControlerPrincipal {
 		}
 		return null;
 	}
-	
+
 	public static void registerNewUser(Usuario newUser) {
 		listOfUsers.add(newUser);
 	}
-	
-	//buscarUsuarioValidado metodo
+
+	// buscarUsuarioValidado metodo
 }
