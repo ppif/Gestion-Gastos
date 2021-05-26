6978fe846f73afee8cbcaf237e08d08ddc7fd7fc
mati <matiCIF@outlook.es>
2021-05-26 12:44:09 +0200

    c3

--- GestionGastos/src/clasesconmetodos/GastosManipulador.java
+++ GestionGastos/src/clasesconmetodos/GastosManipulador.java
@@ -1,5 +1,14 @@
 package clasesconmetodos;
 
+import controler.ControlerPrincipal;
+import modelo.Gasto;
+import modelo.Usuario;
+import java.util.ArrayList;
+
 public class GastosManipulador {
 
+	
+	public static void borrarGastos(String usuario) {
+		ControlerPrincipal.getUserFromList(usuario).setGastos(new ArrayList<Gasto>());
+	}
 }
