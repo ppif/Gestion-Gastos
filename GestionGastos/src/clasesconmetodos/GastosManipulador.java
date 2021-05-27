d17069044e0cf9c3acd957d4e2dce34f650c8f55
mati <mati@DESKTOP-GAKM3OF>
2021-05-27 13:25:49 +0200

    listargasto corregido

--- GestionGastos/src/clasesconmetodos/GastosManipulador.java
+++ GestionGastos/src/clasesconmetodos/GastosManipulador.java
@@ -5,13 +5,13 @@ import modelo.Usuario;
 
 public class GastosManipulador {
 	
-	public static void listarGasto (Scanner reader, Usuario userValidated) {
+	public static void listarGasto (Scanner reader, Usuario usuarioValidated) {
 			
 				
 				int contadorparalistado = 1;
 				System.out.println("\nLista de gastos: ");
 				
-				for (Gasto gasto : userValidated.getGastos()) {
+				for (Gasto gasto : usuarioValidated.getGastos()) {
 
 				System.out.println("- " + contadorparalistado + " " + gasto);
 				contadorparalistado++;
