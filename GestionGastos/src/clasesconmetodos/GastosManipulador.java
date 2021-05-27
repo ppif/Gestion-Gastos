84709357cad0d9fcf44c816aac6a8074234265ff
admin CIFO <admin CIFO@DESKTOP-90KPIC7>
2021-05-27 13:47:36 +0200

    anadir_gasto y salir.

--- GestionGastos/src/clasesconmetodos/GastosManipulador.java
+++ GestionGastos/src/clasesconmetodos/GastosManipulador.java
@@ -1,5 +1,33 @@
 package clasesconmetodos;
 
+
 public class GastosManipulador {
 
-}
+
+				Scanner reader = new Scanner(System.string);
+				System.out.println("Que quiere  ("anadir gasto","salir");
+				String userName = reader.nextLine();
+
+			
+				
+				if (reader.equals("anadir gastos")) {
+					
+					System.out.println("nombre de gasto");
+					
+				
+				string nombreDeGasto = crearGasto.indexOf(" ");
+				
+			    String nombreGasto = crearGasto.substring.ArrayList<Gasto> gastos;
+				
+				return gasto;
+			}
+			
+			
+			if (reader.equals("Salir")) break;
+			}
+	}
+	
+
+			
+
+				
